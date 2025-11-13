576555d95430f7d55a2a99a1c3c67a7948c12921
Lucas Leandro <lucasl_pereira@hotmail.com>
2025-11-13 11:43:21 -0300

    First

---
+++ src/main/java/dev/java/CadastroDeN/CadastroDeNApplication.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroDeN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/CadastroDeN/CadastroDeNApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroDeN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
