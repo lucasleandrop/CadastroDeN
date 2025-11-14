b6ffc8064f219e457bf64ba6562a1fa986626deb
Lucas Leandro <lucasl_pereira@hotmail.com>
2025-11-14 19:03:55 -0300

    fix: adiciona o @Column na minha clas

--- src/main/java/dev/java/CadastroDeN/Ninjas/NinjaModel.java
+++ src/main/java/dev/java/CadastroDeN/Ninjas/NinjaModel.java
@@ -19,15 +19,20 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_Sequencial")
     private Long id;
 
+    @Column(name = "Nome_Ninja")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "Idade")
     private int idade;
 
+    @Column(name = "Img_URL")
+    private String imgUrl;
 
     // Muitos ninjas para uma missão
     @ManyToOne
---
+++ src/main/java/dev/java/CadastroDeN/Ninjas/NinjaRepository.java
@@ -0,0 +1,4 @@
+package dev.java.CadastroDeN.Ninjas;
+
+public class NinjaRepository {
+}
