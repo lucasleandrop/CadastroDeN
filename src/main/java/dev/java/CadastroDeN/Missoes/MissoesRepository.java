82941b7168504328d9d501817357c316fa1977c0
Lucas Leandro <lucasl_pereira@hotmail.com>
2025-11-15 23:07:16 -0300

    build: Conecta o banco de dados a minha aplicacao Spring

--- src/main/java/dev/java/CadastroDeN/Missoes/MissoesRepository.java
+++ src/main/java/dev/java/CadastroDeN/Missoes/MissoesRepository.java
@@ -2,5 +2,5 @@ package dev.java.CadastroDeN.Missoes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissoesRepository extends JpaRepository<MissoesModel,Long > {
+public interface MissoesRepository extends JpaRepository<MissoesModel,Long> {
 }
