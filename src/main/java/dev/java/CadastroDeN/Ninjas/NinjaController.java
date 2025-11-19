26c19d4e9c926070fc121ca9e24351e03a8d8114
Lucas Leandro <lucasl_pereira@hotmail.com>
2025-11-19 12:51:24 -0300

    feat: Altera a rota do controller para listar os ninjas e injeta a dependencia do service e do repository.

--- src/main/java/dev/java/CadastroDeN/Ninjas/NinjaController.java
+++ src/main/java/dev/java/CadastroDeN/Ninjas/NinjaController.java
@@ -1,17 +1,52 @@
 package dev.java.CadastroDeN.Ninjas;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
-    @GetMapping("/boasvindas")
-    public String boasVindas(){
-        return "Essa é a minha primeira mensagem nessa rota.";
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
     }
 
-}
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é a minha primeira mensagem nessa rota.";
+    }
+        //Adicionar / criar
+        @PostMapping("/criar")
+        public String criarNinhja() {
+            return "Ninja Criado";
+        }
+            //Mostrar
+            @GetMapping("/todos")
+                    public List<NinjaModel> mostrarTodos (){
+                return ninjaService.mostrarTodos();
+            }
+
+        //Procurar
+        @GetMapping("/todosID")
+    public String mostrarTodosPorId() {
+    return "Mostrar todos por ID";
+    }
+
+        //Alterar
+
+        @PutMapping("/alterar")
+    public String alterarPorId() {
+        return "Alterar por ID";
+    }
+
+        //Deletar
+        @DeleteMapping("/deletarID")
+    public String deletarPorId() {
+            return "Deletado por ID";
+        }
+
+    }
--- src/main/java/dev/java/CadastroDeN/Ninjas/NinjaService.java
+++ src/main/java/dev/java/CadastroDeN/Ninjas/NinjaService.java
@@ -1,4 +1,25 @@
 package dev.java.CadastroDeN.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+
+    //Listar todos
+    public List<NinjaModel> mostrarTodos (){
+        return ninjaRepository.findAll();
+    }
+
+
+
 }
